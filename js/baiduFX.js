a81ed256f440f828264fe763f3d955f9e833c962
DxSummer <2088809753@qq.com>
2020-10-04 16:03:09 +0800

    Site updated: 2020-10-04 16:03:08

---
+++ js/baiduFX.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?169c1f89fc8d83fbd4633b82f857c75f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
