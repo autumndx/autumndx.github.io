af75c8b9191122d7a373053fc5aaba0446f72c97
DxSummer <2088809753@qq.com>
2020-10-11 11:52:42 +0800

    Site updated: 2020-10-11 11:52:41

---
+++ js/clipboard-use.js
@@ -0,0 +1,13 @@
+!function (e, t, a) {
+    /* code */
+    var initCopyCode = function () {
+      var copyHtml = '<button class="btn-copy" data-clipboard-snippet=""><i class="fa fa-clipboard"></i></button>';
+      $(".highlight .code pre").before(copyHtml);
+      new ClipboardJS('.btn-copy', {
+        target: function (trigger) {
+          return trigger.nextElementSibling;
+        }
+      });
+    }
+    initCopyCode();
+  }(window, document);
